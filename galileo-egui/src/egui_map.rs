96e96b084cd30ce9930fc793a376b5b5d56a302e
Joshua Milas <josh.milas@gmail.com>
2025-11-23 11:01:52 -0500

    Cleanup of match statement

--- galileo-egui/src/egui_map.rs
+++ galileo-egui/src/egui_map.rs
@@ -322,18 +322,10 @@ impl<'a> EguiMapState {
             Event::Touch { device_id: _, id, phase, pos, force: _ } => {
                 let event = TouchEvent { touch_id: id.0, position: Point2::new(pos.x as f64, pos.y as f64) };
                 match phase {
-                    egui::TouchPhase::Start => {
-                        Some(RawUserEvent::TouchStart(event))
-                    },
-                    egui::TouchPhase::Move => {
-                        Some(RawUserEvent::TouchMove(event))
-                    },
-                    egui::TouchPhase::End => {
-                        Some(RawUserEvent::TouchEnd(event))
-                    },
-                    egui::TouchPhase::Cancel => {
-                        Some(RawUserEvent::TouchEnd(event))
-                    },
+                    egui::TouchPhase::Start => Some(RawUserEvent::TouchStart(event)),
+                    egui::TouchPhase::Move => Some(RawUserEvent::TouchMove(event)),
+                    egui::TouchPhase::End => Some(RawUserEvent::TouchEnd(event)),
+                    egui::TouchPhase::Cancel => Some(RawUserEvent::TouchEnd(event)),
                 }
             }
 
